a604c727b9f41e2874c8fb9cac93eb1acfed0582
wwwyssa <grisha.maryin@gmail.com>
2023-10-31 16:55:48 +0500

    smth done

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ class MyGeomHelper(QMainWindow):
         self.testing_start_btn.clicked.connect(self.move_to_another)
         self.l_back_btn.clicked.connect(self.move_to_another)
         self.t_back_btn.clicked.connect(self.move_to_another)
+        self.pushButton_learn.clicked.connect(self.show_picture)
 
     def launch(self):
         self.set_background()
-        self.fill_comboBox()
+        self.fill_combobox()
 
     def set_background(self):
         name = 'load_files/back.png'
@@ -39,11 +40,19 @@ class MyGeomHelper(QMainWindow):
             self.stackedWidget.setCurrentIndex(3)
         if self.sender().text() == "НАЗАД НА ГЛАВНУЮ":
             self.stackedWidget.setCurrentIndex(0)
+            self.comboBox_learn.clear()
+            name = f'load_files/clear.png'
+            self.curr_image1 = QImage(name)
+            self.patimg1 = QImage(name)
+            self.pixmap1 = QPixmap(f'{name}')
+            self.lable_learn.resize(400, 185)
+            self.lable_learn.setPixmap(self.pixmap1)
         if self.sender().text() == "НАЧАТЬ ТЕСТИРОВАНИЕ":
             self.test_prepearing()
             self.stackedWidget.setCurrentIndex(4)
         if self.sender().text() == "НАЧАТЬ ОБУЧЕНИЕ":
             self.stackedWidget.setCurrentIndex(2)
+            self.learn_preparing()
 
     def fill_combobox(self):
         with open('load_files/shapes.txt', 'r', encoding='utf-8') as f:
@@ -57,6 +66,24 @@ class MyGeomHelper(QMainWindow):
         self.user_name = self.get_name.text()
         self.get_name.clear()
         self.test_theme = self.comboBox_theme.currentText()
+
+    def show_picture(self):
+        shape = self.comboBox.currentText().rstrip()
+        theme = self.comboBox_learn.currentText().rstrip()
+        name = f'load_files/{shape}/{theme}.png'
+        self.curr_image1 = QImage(name)
+        self.patimg1 = QImage(name)
+        self.pixmap1 = QPixmap(f'{name}')
+        self.lable_learn.resize(400, 185)
+        self.lable_learn.setPixmap(self.pixmap1)
+
+    def learn_preparing(self):
+        shape = self.comboBox.currentText().rstrip()
+        with open(f"load_files/{shape}/{shape}.txt", "r", encoding='utf-8') as f:
+            text = f.readlines()
+        self.info_learn.setText(text[0])
+        for x in text[1::]:
+            self.comboBox_learn.addItem(x)
 
 
 if __name__ == '__main__':
